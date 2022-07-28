da2e5073489971fc94c70cf7aa94de8a9c12df50
Mori <sayedmurtazamuttahary@gmail.com>
2022-07-28 10:32:39 +0200

    fixed jumping body tag

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-// const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:4321/" : "https://final-project-teacher-app.herokuapp.com/";
 const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:4321/" : "https://final-project-teacher-app.herokuapp.com/";
 
 const clientId = '7633690609-nm7dhj6md96naeal4uga22iof0dv4rlr.apps.googleusercontent.com';
