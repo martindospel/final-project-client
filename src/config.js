7888788f21ddc1858234824aecb41582da4cb0df
Martin Dospel <89632465+martindospel@users.noreply.github.com>
2023-02-16 09:18:36 +0100

    change clientID

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:4321/" : "https://trackmate.cyclic.app/";
 
-const clientId = '7633690609-nm7dhj6md96naeal4uga22iof0dv4rlr.apps.googleusercontent.com';
+const clientId = '419994356338-s6506op6ac51n17mbviuhi5di8587da1.apps.googleusercontent.com';
 
 export { baseUrl, clientId };
