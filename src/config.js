282cca0c195a3bccb118190ae3087bc54d5fb340
Martin Dospel <89632465+martindospel@users.noreply.github.com>
2023-02-16 09:29:24 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:4321/" : "https://perfect-wasp-shorts.cyclic.app";
+const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:4321/" : "https://trackmate.cyclic.app";
 
 const clientId = '419994356338-s6506op6ac51n17mbviuhi5di8587da1.apps.googleusercontent.com';
 
